79c3bca6a7c36e1754bf200adbe7249e48446abd
André Quintino <andre.quintino@mindswap.academy>
2022-11-05 12:05:02 +0000

    feat(utils): implement set cookie function

---
+++ src/utils/setCookie/setCookie.ts
@@ -0,0 +1,15 @@
+function setCookie(
+	name: string,
+	value: string,
+	expirationInSeconds: number
+): void {
+	const date = new Date();
+
+	date.setTime(date.getTime() + expirationInSeconds * 1000);
+
+	const expires = 'expires=' + date.toUTCString();
+
+	document.cookie = name + '=' + value + '; ' + expires + '; path=/';
+}
+
+export { setCookie };
