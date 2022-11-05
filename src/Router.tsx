fe6eefb7cd859f273d0b8340ea16ba30251af89a
André Quintino <andre.quintino@mindswap.academy>
2022-11-05 14:47:56 +0000

    feat(router): implement private route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
 import { UserProfile } from './pages/private';
 import { Home, Login } from './pages/public';
+import { getTokenFromCookies } from './utils';
+
+interface IPrivateRouteProps {
+	redirectPath: string;
+}
 
 function Router(): JSX.Element {
+	function PrivateRoute({ redirectPath }: IPrivateRouteProps): JSX.Element {
+		return getTokenFromCookies().length > 0 ? (
+			<Outlet />
+		) : (
+			<Navigate to={redirectPath} replace />
+		);
+	}
+
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/login" element={<Login />} />
-			<Route path="/userprofile" element={<UserProfile />} />
+			<Route element={<PrivateRoute redirectPath="/" />}>
+				<Route path="/userprofile" element={<UserProfile />} />
+			</Route>
 		</Routes>
 	);
 }
