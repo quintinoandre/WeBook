907737f4c8dfb22d0fe8fd9f24ba343edded501f
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-11-09 23:06:31 +0000

    feat (footer): install footer

---
+++ src/components/Footer/Text.tsx
@@ -0,0 +1,19 @@
+function Text(): JSX.Element {
+	return (
+		<h1>
+			The more often we see the things around us - even the beautiful and
+			wonderful things - the more they become invisible to us. That is why
+			we often take for granted the beauty of this world: the flowers, the
+			trees, the birds, the clouds - even those we love. Because we see
+			things so often, we see them less and less. We know that we are
+			often judged by the company we keep. We know how influential
+			classmates, friends, and other peer groups can be. If any of our
+			companions are prone to be unrighteous in their living, we are
+			better off seeking new associations immediately. Between stimulus
+			and response there is a space. In that space is our power to choose
+			our response. In our response lies our growth and our freedom.
+		</h1>
+	);
+}
+
+export { Text };
