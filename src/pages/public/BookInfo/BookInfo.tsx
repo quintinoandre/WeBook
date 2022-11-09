e4a8406067106127841f05f5d3319a02e4fe6ec0
André Quintino <andre.quintino@mindswap.academy>
2022-11-09 00:52:43 +0000

    feat(layouts): implement header

--- src/pages/public/BookInfo/BookInfo.tsx
+++ src/pages/public/BookInfo/BookInfo.tsx
@@ -1,11 +1,9 @@
-import { Navbar } from '../../../components/Navbar';
 import { BookInfoContainter } from './BookContainer';
 import { Form } from './Form';
 
 function BookInfo(): JSX.Element {
 	return (
 		<>
-			<Navbar />
 			<BookInfoContainter />
 			{/* <Carousel/> */}
 			<Form />
